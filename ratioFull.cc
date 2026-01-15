d73c76e965fad949c2124081bd19b9243cb1d4cb
mochmans <michal@DESKTOP-6OPMSMC.localdomain>
2026-01-15 17:36:55 +0100

    cf update

--- ratioFull.cc
+++ ratioFull.cc
@@ -10,17 +10,20 @@
 #include <TRandom.h>
 #include "TFitResultPtr.h"
 #include "TFitResult.h"
+#include "TLegend.h"
 #include "TDirectory.h"
 
 void configureHistogram(TH1* histo, Color_t markerColor, Style_t markerStyle, Size_t markerSize){
     histo->SetMarkerColor(markerColor);
     histo->SetMarkerStyle(markerStyle);
     histo->SetMarkerSize(markerSize);
-    histo->GetXaxis()->SetRangeUser(0,3);
+    histo->GetXaxis()->SetRangeUser(0,1.5);
     histo->Scale(1.0);
 }
 
-TH1F *createCFHistogram(TH1F* hNumerator, TH1F* hDenominator, const char* name){
+TH1F *createCFHistogram(TH1F* hNumerator, TH1F* hDenominator, const char* name, Int_t rebinFactor=1){
+    // hNumerator->Rebin(rebinFactor);
+    // hDenominator->Rebin(rebinFactor);
     TH1F *hCF = (TH1F*)hNumerator->Clone(name);
     hCF->Divide(hDenominator);
     return hCF;
@@ -41,7 +44,7 @@ void normalizeHistogram(TH1F* histo, Double_t kstarLower, Double_t kstarUpper){
 int ratioFull(){
     TH1::SetDefaultSumw2();
 
-    TFile* f = new TFile("mediumDataOmegaAR2.root","open");
+    TFile* f = new TFile("mediumDataOmegaARfull.root","open");
     
 
 
@@ -97,48 +100,60 @@ int ratioFull(){
     TH1D *hkstarMixedEventpAo50_100 = hAllCentrMixedEventpAo->ProjectionX("hkstarMixedEventpAo50_100",7,9);
 
 
+    Int_t rebinFactor = 3;
 
+    hkstarMixedEvent0_10->Rebin(rebinFactor);
+    hkstarMixedEvent10_20->Rebin(rebinFactor);
+    hkstarMixedEvent20_30->Rebin(rebinFactor);
+    hkstarMixedEvent30_50->Rebin(rebinFactor);
+    hkstarMixedEvent50_100->Rebin(rebinFactor);
+    hMixedEventRatio->Rebin(rebinFactor);
+    hkstarMixedEventpAo0_10->Rebin(rebinFactor);
+    hkstarMixedEventpAo10_20->Rebin(rebinFactor);
+    hkstarMixedEventpAo20_30->Rebin(rebinFactor);
+    hkstarMixedEventpAo30_50->Rebin(rebinFactor);
+    hkstarMixedEventpAo50_100->Rebin(rebinFactor);
+    hMixedEventRatiopAo->Rebin(rebinFactor);
+    hSameEventRatio->Rebin(rebinFactor);
+    hkstarSameEvent0_10->Rebin(rebinFactor);
+    hkstarSameEvent10_20->Rebin(rebinFactor);
+    hkstarSameEvent20_30->Rebin(rebinFactor);
+    hkstarSameEvent30_50->Rebin(rebinFactor);
+    hkstarSameEvent50_100->Rebin(rebinFactor);
+    hkstarSameEventpAo0_10->Rebin(rebinFactor);
+    hkstarSameEventpAo10_20->Rebin(rebinFactor);
+    hkstarSameEventpAo20_30->Rebin(rebinFactor);
+    hkstarSameEventpAo30_50->Rebin(rebinFactor);
+    hkstarSameEventpAo50_100->Rebin(rebinFactor);
+    hSameEventRatiopAo->Rebin(rebinFactor);
    
-    TH1F* hCFall = createCFHistogram((TH1F*)hSameEventRatio, (TH1F*)hMixedEventRatio, "hCFunpbar");   //antyproton omega
+    TH1F* hCFall = createCFHistogram((TH1F*)hSameEventRatio, (TH1F*)hMixedEventRatio, "hCFunpbar", rebinFactor);   //antyproton omega
     
-    TH1F* hCF0_10 = createCFHistogram((TH1F*)hkstarSameEvent0_10, (TH1F*)hkstarMixedEvent0_10, "hCF0_10pbar");
+    TH1F* hCF0_10 = createCFHistogram((TH1F*)hkstarSameEvent0_10, (TH1F*)hkstarMixedEvent0_10, "hCF0_10pbar", rebinFactor);
     
-    TH1F* hCF10_20 = createCFHistogram((TH1F*)hkstarSameEvent10_20, (TH1F*)hkstarMixedEvent10_20, "hCF10_20pbar");
+    TH1F* hCF10_20 = createCFHistogram((TH1F*)hkstarSameEvent10_20, (TH1F*)hkstarMixedEvent10_20, "hCF10_20pbar", rebinFactor);
     
-    TH1F* hCF20_30 = createCFHistogram((TH1F*)hkstarSameEvent20_30, (TH1F*)hkstarMixedEvent20_30, "hCF20_30pbar");
+    TH1F* hCF20_30 = createCFHistogram((TH1F*)hkstarSameEvent20_30, (TH1F*)hkstarMixedEvent20_30, "hCF20_30pbar", rebinFactor);
     
-    TH1F* hCF30_50 = createCFHistogram((TH1F*)hkstarSameEvent30_50, (TH1F*)hkstarMixedEvent30_50, "hCF30_50pbar");
+    TH1F* hCF30_50 = createCFHistogram((TH1F*)hkstarSameEvent30_50, (TH1F*)hkstarMixedEvent30_50, "hCF30_50pbar", rebinFactor);
     
-    TH1F* hCF50_100 = createCFHistogram((TH1F*)hkstarSameEvent50_100, (TH1F*)hkstarMixedEvent50_100, "hCF50_100pbar");
+    TH1F* hCF50_100 = createCFHistogram((TH1F*)hkstarSameEvent50_100, (TH1F*)hkstarMixedEvent50_100, "hCF50_100pbar", rebinFactor);
     
 
 
-    TH1F* hCFallpAo = createCFHistogram((TH1F*)hSameEventRatiopAo, (TH1F*)hMixedEventRatiopAo, "hCFunp");  //proton Antyomega
+    TH1F* hCFallpAo = createCFHistogram((TH1F*)hSameEventRatiopAo, (TH1F*)hMixedEventRatiopAo, "hCFunp",rebinFactor);  //proton Antyomega
     
-    TH1F* hCF0_10pAo = createCFHistogram((TH1F*)hkstarSameEventpAo0_10, (TH1F*)hkstarMixedEventpAo0_10, "hCF0_10p");
+    TH1F* hCF0_10pAo = createCFHistogram((TH1F*)hkstarSameEventpAo0_10, (TH1F*)hkstarMixedEventpAo0_10, "hCF0_10p", rebinFactor);
     
-    TH1F* hCF10_20pAo = createCFHistogram((TH1F*)hkstarSameEventpAo10_20, (TH1F*)hkstarMixedEventpAo10_20, "hCF10_20p");
+    TH1F* hCF10_20pAo = createCFHistogram((TH1F*)hkstarSameEventpAo10_20, (TH1F*)hkstarMixedEventpAo10_20, "hCF10_20p", rebinFactor);
     
-    TH1F* hCF20_30pAo = createCFHistogram((TH1F*)hkstarSameEventpAo20_30, (TH1F*)hkstarMixedEventpAo20_30, "hCF20_30p");
+    TH1F* hCF20_30pAo = createCFHistogram((TH1F*)hkstarSameEventpAo20_30, (TH1F*)hkstarMixedEventpAo20_30, "hCF20_30p", rebinFactor);
     
-    TH1F* hCF30_50pAo = createCFHistogram((TH1F*)hkstarSameEventpAo30_50, (TH1F*)hkstarMixedEventpAo30_50, "hCF30_50p");
+    TH1F* hCF30_50pAo = createCFHistogram((TH1F*)hkstarSameEventpAo30_50, (TH1F*)hkstarMixedEventpAo30_50, "hCF30_50p", rebinFactor);
     
-    TH1F* hCF50_100pAo = createCFHistogram((TH1F*)hkstarSameEventpAo50_100, (TH1F*)hkstarMixedEventpAo50_100, "hCF50_100p");
+    TH1F* hCF50_100pAo = createCFHistogram((TH1F*)hkstarSameEventpAo50_100, (TH1F*)hkstarMixedEventpAo50_100, "hCF50_100p", rebinFactor);
      
 
-    Int_t rebinF = 3;
-    hCFall->Rebin(rebinF);
-    hCF0_10->Rebin(rebinF);
-    hCF10_20->Rebin(rebinF);
-    hCF20_30->Rebin(rebinF);
-    hCF30_50->Rebin(rebinF);
-    hCF50_100->Rebin(rebinF);
-    hCFallpAo->Rebin(rebinF);
-    hCF0_10pAo->Rebin(rebinF);
-    hCF10_20pAo->Rebin(rebinF);
-    hCF20_30pAo->Rebin(rebinF);
-    hCF30_50pAo->Rebin(rebinF);
-    hCF50_100pAo->Rebin(rebinF);
 
     configureHistogram(hCFall, kBlue, kOpenCircle, 0.5);
     configureHistogram(hCF0_10, kBlue, kOpenCircle, 0.5);
@@ -169,32 +184,210 @@ int ratioFull(){
     TCanvas *c1 = new TCanvas("pbarOmega","pbarOmega",800,600);
     c1->Divide(2,3);
     c1->cd(1);
+    hCFall->GetYaxis()->SetRangeUser(0.4,1.6);
     hCFall->Draw();
     c1->cd(2);
+    hCF0_10->GetYaxis()->SetRangeUser(0.4,1.6);
     hCF0_10->Draw();
     c1->cd(3);
+    hCF10_20->GetYaxis()->SetRangeUser(0.4,1.6);
     hCF10_20->Draw();
     c1->cd(4);
+    hCF20_30->GetYaxis()->SetRangeUser(0.4,1.6);
     hCF20_30->Draw();
     c1->cd(5);
+    hCF30_50->GetYaxis()->SetRangeUser(0.4,1.6);
     hCF30_50->Draw();
     c1->cd(6);
+    hCF50_100->GetYaxis()->SetRangeUser(0,3);
     hCF50_100->Draw();
 
     TCanvas *c2 = new TCanvas("pOmegabar","pOmegabar",800,600);
     c2->Divide(2,3);
     c2->cd(1);
+    hCFallpAo->GetYaxis()->SetRangeUser(0.4,1.6);
     hCFallpAo->Draw();
     c2->cd(2);
+    hCF0_10pAo->GetYaxis()->SetRangeUser(0.4,1.6);
     hCF0_10pAo->Draw();
     c2->cd(3);
+    hCF10_20pAo->GetYaxis()->SetRangeUser(0.4,1.6);
     hCF10_20pAo->Draw();
     c2->cd(4);
+    hCF20_30pAo->GetYaxis()->SetRangeUser(0.4,1.6);
     hCF20_30pAo->Draw();
     c2->cd(5);
+    hCF30_50pAo->GetYaxis()->SetRangeUser(0.4,1.6);
     hCF30_50pAo->Draw();
     c2->cd(6);
+    hCF50_100pAo->GetYaxis()->SetRangeUser(0,3);
     hCF50_100pAo->Draw(); 
+
+    double_t msize = 0.25;
+
+    TCanvas *c3 = new TCanvas("events proton antiomega","Comparison",800,600);
+    c3->Divide(2,3);
+    c3->cd(1);
+    hSameEventRatiopAo->SetMarkerColor(kBlue);
+    hSameEventRatiopAo->GetXaxis()->SetRangeUser(0,4);
+    hSameEventRatiopAo->Draw();
+    hMixedEventRatiopAo->SetMarkerStyle(kOpenSquare);
+    hMixedEventRatiopAo->SetMarkerColor(kRed);
+    hMixedEventRatiopAo->SetMarkerSize(msize);
+    hMixedEventRatiopAo->Draw("SAME");
+
+    TLegend *legend1 = new TLegend();
+    legend1->AddEntry(hkstarSameEventpAo0_10,"Same Event","p");
+    legend1->AddEntry(hkstarMixedEventpAo0_10,"Mixed Event","p");
+    legend1->Draw();
+
+    
+
+    c3->cd(2);  
+    hkstarSameEventpAo0_10->SetMarkerColor(kBlue);
+    hkstarSameEventpAo0_10->GetXaxis()->SetRangeUser(0,4);
+    hkstarSameEventpAo0_10->Draw();
+    hkstarMixedEventpAo0_10->SetMarkerStyle(kOpenSquare);
+    hkstarMixedEventpAo0_10->SetMarkerColor(kRed);
+    hkstarMixedEventpAo0_10->SetMarkerSize(msize);
+    hkstarMixedEventpAo0_10->Draw("SAME");
+    c3->cd(3);
+    hkstarSameEventpAo10_20->SetMarkerColor(kBlue);
+    hkstarSameEventpAo10_20->GetXaxis()->SetRangeUser(0,4);
+    hkstarSameEventpAo10_20->Draw();
+    hkstarMixedEventpAo10_20->SetMarkerStyle(kOpenSquare);
+    hkstarMixedEventpAo10_20->SetMarkerColor(kRed);
+    hkstarMixedEventpAo10_20->SetMarkerSize(msize);
+    hkstarMixedEventpAo10_20->Draw("SAME");
+    c3->cd(4);
+    hkstarSameEventpAo20_30->SetMarkerColor(kBlue);
+    hkstarSameEventpAo20_30->GetXaxis()->SetRangeUser(0,4);
+    hkstarSameEventpAo20_30->Draw();
+    hkstarMixedEventpAo20_30->SetMarkerStyle(kOpenSquare);
+    hkstarMixedEventpAo20_30->SetMarkerColor(kRed);
+    hkstarMixedEventpAo20_30->SetMarkerSize(msize);
+    hkstarMixedEventpAo20_30->Draw("SAME");
+    c3->cd(5);
+    hkstarSameEventpAo30_50->SetMarkerColor(kBlue);
+    hkstarSameEventpAo30_50->GetXaxis()->SetRangeUser(0,4);
+    hkstarSameEventpAo30_50->Draw();
+    hkstarMixedEventpAo30_50->SetMarkerStyle(kOpenSquare);
+    hkstarMixedEventpAo30_50->SetMarkerColor(kRed);
+    hkstarMixedEventpAo30_50->SetMarkerSize(msize);
+    hkstarMixedEventpAo30_50->Draw("SAME");
+    c3->cd(6);
+    hkstarSameEventpAo50_100->SetMarkerColor(kBlue);
+    hkstarSameEventpAo50_100->GetXaxis()->SetRangeUser(0,4);
+    hkstarSameEventpAo50_100->Draw();
+    hkstarMixedEventpAo50_100->SetMarkerStyle(kOpenSquare);
+    hkstarMixedEventpAo50_100->SetMarkerColor(kRed);
+    hkstarMixedEventpAo50_100->SetMarkerSize(msize);
+    hkstarMixedEventpAo50_100->Draw("SAME");
+
+    
+
+    TCanvas *c4 = new TCanvas("events antiproton omega","k* Tcomparison",800,600);
+    c4->Divide(2,3);
+    c4->cd(1);
+    hSameEventRatio->SetMarkerColor(kBlue);
+    hSameEventRatio->GetXaxis()->SetRangeUser(0,4);
+    hSameEventRatio->Draw();
+    hMixedEventRatio->SetMarkerStyle(kOpenSquare);
+    hMixedEventRatio->SetMarkerColor(kRed);
+    hMixedEventRatio->SetMarkerSize(msize);
+    hMixedEventRatio->Draw("SAME");
+    c4->cd(2);  
+    hkstarSameEvent0_10->SetMarkerColor(kBlue);   
+    hkstarSameEvent0_10->GetXaxis()->SetRangeUser(0,4);
+    hkstarSameEvent0_10->Draw();
+    hkstarMixedEvent0_10->SetMarkerStyle(kOpenSquare);
+    hkstarMixedEvent0_10->SetMarkerColor(kRed);
+    hkstarMixedEvent0_10->SetMarkerSize(msize);
+    hkstarMixedEvent0_10->Draw("SAME"); 
+    c4->cd(3);
+    hkstarSameEvent10_20->SetMarkerColor(kBlue);
+    hkstarSameEvent10_20->GetXaxis()->SetRangeUser(0,4);
+    hkstarSameEvent10_20->Draw();
+    hkstarMixedEvent10_20->SetMarkerStyle(kOpenSquare);
+    hkstarMixedEvent10_20->SetMarkerColor(kRed);
+    hkstarMixedEvent10_20->SetMarkerSize(msize);
+    hkstarMixedEvent10_20->Draw("SAME");
+    c4->cd(4);
+    hkstarSameEvent20_30->SetMarkerColor(kBlue);
+    hkstarSameEvent20_30->GetXaxis()->SetRangeUser(0,4);
+    hkstarSameEvent20_30->Draw();
+    hkstarMixedEvent20_30->SetMarkerStyle(kOpenSquare);
+    hkstarMixedEvent20_30->SetMarkerColor(kRed);
+    hkstarMixedEvent20_30->SetMarkerSize(msize);
+    hkstarMixedEvent20_30->Draw("SAME");    
+    c4->cd(5);
+    hkstarSameEvent30_50->SetMarkerColor(kBlue);
+    hkstarSameEvent30_50->GetXaxis()->SetRangeUser(0,4);
+    hkstarSameEvent30_50->Draw();
+    hkstarMixedEvent30_50->SetMarkerStyle(kOpenSquare);
+    hkstarMixedEvent30_50->SetMarkerColor(kRed);
+    hkstarMixedEvent30_50->SetMarkerSize(msize);
+    hkstarMixedEvent30_50->Draw("SAME");
+    c4->cd(6);
+    hkstarSameEvent50_100->SetMarkerColor(kBlue);
+    hkstarSameEvent50_100->GetXaxis()->SetRangeUser(0,4);
+    hkstarSameEvent50_100->Draw();
+    hkstarMixedEvent50_100->SetMarkerStyle(kOpenSquare);
+    hkstarMixedEvent50_100->SetMarkerColor(kRed);
+    hkstarMixedEvent50_100->SetMarkerSize(msize);
+    hkstarMixedEvent50_100->Draw("SAME");
+
+    TLegend *legend2 = new TLegend();
+    legend2->AddEntry(hkstarSameEvent0_10,"Same Event","p");
+    legend2->AddEntry(hkstarMixedEvent0_10,"Mixed Event","p");
+    c4->cd(1);
+    legend2->Draw();
+
+    TCanvas *c5 = new TCanvas("Cf comparison","Cf comparison",800,600);
+    c5->Divide(2,3);
+    c5->cd(1);
+    hCFall->SetMarkerColor(kBlue);
+    hCFall->Draw();
+    hCFallpAo->SetMarkerStyle(kOpenSquare);
+    hCFallpAo->SetMarkerColor(kRed);
+    hCFallpAo->Draw("SAME");
+    c5->cd(2);
+    hCF0_10->SetMarkerColor(kBlue);
+    hCF0_10->Draw();
+    hCF0_10pAo->SetMarkerStyle(kOpenSquare);
+    hCF0_10pAo->SetMarkerColor(kRed);
+    hCF0_10pAo->Draw("SAME");
+    c5->cd(3);
+    hCF10_20->SetMarkerColor(kBlue);
+    hCF10_20->Draw();
+    hCF10_20pAo->SetMarkerStyle(kOpenSquare);
+    hCF10_20pAo->SetMarkerColor(kRed);
+    hCF10_20pAo->Draw("SAME");
+    c5->cd(4);
+    hCF20_30->SetMarkerColor(kBlue);
+    hCF20_30->Draw();
+    hCF20_30pAo->SetMarkerStyle(kOpenSquare);
+    hCF20_30pAo->SetMarkerColor(kRed);
+    hCF20_30pAo->Draw("SAME");
+    c5->cd(5);
+    hCF30_50->SetMarkerColor(kBlue);
+    hCF30_50->Draw();
+    hCF30_50pAo->SetMarkerStyle(kOpenSquare);
+    hCF30_50pAo->SetMarkerColor(kRed);
+    hCF30_50pAo->Draw("SAME");
+    c5->cd(6);
+    hCF50_100->SetMarkerColor(kBlue);
+    hCF50_100->Draw();
+    hCF50_100pAo->SetMarkerStyle(kOpenSquare);
+    hCF50_100pAo->SetMarkerColor(kRed); 
+    hCF50_100pAo->Draw("SAME");
+
+    TLegend *legend3 = new TLegend();
+    legend3->AddEntry(hCFall,"antiproton-omega","p");
+    legend3->AddEntry(hCFallpAo,"proton-antiomega","p");
+    c5->cd(1);
+    legend3->Draw();
+
 
     
     return 0; 
